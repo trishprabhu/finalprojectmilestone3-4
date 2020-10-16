57083f139d05127ee71c30b31f1c49226d4afe52
Trisha Prabhu <trishaprabhu@Trishas-MacBook-Pro.local>
2020-10-15 19:04:59 -0500

    Commit!

--- finalprojectmilestone3-4/app.R
+++ finalprojectmilestone3-4/app.R
@@ -2,14 +2,14 @@ library(shiny)
 library(readr)
 library(sentimentr)
 
-# Read in Trump Twitter data:
+# Read in Donald Trump Twitter data:
 
-trumptweets <- read_csv("data/Trump_tweets (1).csv")
+trumptweets <- read_csv("Trump_tweets (1).csv")
 summary(trumptweets)
 
-# Read in Hillary Twitter data:
+# Read in Hillary Clinton Twitter data:
 
-hillarytweets <- read_csv("data/hillarytweets.csv")
+hillarytweets <- read_csv("hillarytweets.csv")
 summary(hillarytweets)
 
 # Sentiment values:
@@ -20,33 +20,51 @@ hillary_sentiment_scores <- sentiment(hillarytweets$text[1:100])
 # Define UI:
 
 ui <- navbarPage(
-    "Trisha's Final Project Milestone #4",
-    tabPanel("Sentiment Analysis",
+    "Trisha's Final Project Milestones",
+    tabPanel("Tweet Sentiment Analysis",
              fluidPage(
                  titlePanel("Sentiment Analysis"),
                  sidebarLayout(
                      sidebarPanel(
                          selectInput(inputId = "dataset",
-                                     label = "Choose a dataset:",
-                                     choices = c("Hillary", "Trump")),
+                                     label = "Choose a Twitter account:",
+                                     choices = c("Hillary Clinton", 
+                                                 "Donald Trump")),
                          numericInput(inputId = "obs",
                                       label = "Number of observations to view:",
                                       value = 10),
-                         sliderInput("obs", "Number of observations  to view:",
+                         sliderInput("obs", "Slide to the number of
+                                     observations to view:",
                                          min = 0, max = 300, value = 30
                              )),
                      mainPanel(
                          verbatimTextOutput("summary"),
                          tableOutput("view"),
-                         
                      )))),
     tabPanel("Discussion",
              titlePanel("About The Data"),
-             p("Once my data is finalized, I'll complete this section.")),
+             p("09/16 Status Report: The data used in this project currently 
+             consists of two datasets -- Donald Trump's Tweets, from 07/13/20 to 
+             10/13/20, and Hillary Clinton's Tweets, from 08/03/16 to 11/03/16 
+             (both meant to represent either 3 months before their respective
+             Presidential elections/3 months before the present date). I've
+             currently run a sentiment analysis on a subset of observations;
+             in the future, I plan to both 1) run additional analyses with
+             regards to sentiment (e.g. how does sentiment vary with time
+             (closer to the election)?) and 2) look at other elements of the
+             Tweets, from their characteristics (does Donald Trump Retweet
+             more, or write his own Tweets more?) to their correlation
+             with other data of interest (such as Trump's approval rating).
+             In doing this, I plan to focus in more exclusively on Donald
+             Trump (as opposed to Hillary Clinton), and to pull/clean data
+             from Twitter's API."), 
+             a("See the data currently in use by visiting this Dropbox link.", 
+               href = "https://www.dropbox.com/sh/5azksa5cvrsi9cs/AADvM-p9h8Sqf4oYzcgaMWXda?dl=0")
+    ),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
-             p("Hello! This is a project exploring Donald Trump's tweets,
+             p("Hello! This is a project exploring Donald Trump's Tweets,
                and what they can tell us about his Presidency, politics,
                and the world at large."),
              h3("About Me"),
@@ -64,8 +82,8 @@ server <- function(input, output) {
     # Return the requested data-set ----
     datasetInput <- reactive({
         switch(input$dataset,
-               "Hillary" = hillary_sentiment_scores,
-               "Trump" = trump_sentiment_scores)
+               "Hillary Clinton" = hillary_sentiment_scores,
+               "Donald Trump" = trump_sentiment_scores)
     })
     
     # Generate a summary of the dataset ----
@@ -77,10 +95,6 @@ server <- function(input, output) {
     # Show the first "n" observations ----
     output$view <- renderTable({
         head(datasetInput(), n = input$obs)
-    })
-    
-    output$distPlot <- renderPlot({
-        hist(rnorm(input$obs))
     })
     
 }
