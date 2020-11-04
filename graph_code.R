820b822e6b21d700b4fc464d3bad04bd8229a5d1
Trisha Prabhu <trishaprabhu@Trishas-MacBook-Pro.local>
2020-11-04 15:04:00 -0600

    Commit!

--- graph_code.R
+++ graph_code.R
@@ -1,7 +1,10 @@
 # Load the dplyr and sentimentr library.
 
+library(magrittr)
 library(dplyr)
 library(sentimentr)
+library(tidyverse)
+library(readr)
 
 # Load the lubridate library; use as.Date to create a new column with the
 # created_at values in MDY format.
@@ -32,10 +35,6 @@ View(approval_polllist)
 # Create a vector of ending dates to iterate over.
 
 enddates <- unique(approval_polllist$enddate)
-
-#Create an empty column to deposit approval ratings in.
-
-trumptweets$approvalpolls <- c()
   
 #For loop is below:
 
@@ -45,7 +44,13 @@ n
 results <- rep(NA, n)
 
 for (i in 1:n) {
-  results[i] <- approval_polllist$approve[i]
+  filter(approval_polllist$enddate == enddates[i]) %>%
+  results[i] <- mean(approval_polllist$approve)
   #Pull the approval polls from approval_pollist and put in trump_tweets
 }
 results
+
+approval_polllist %>%
+  filter(enddate == enddates[1]) %>%
+  mean([, ])
+  
