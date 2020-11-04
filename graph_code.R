2bc5149580b9da4913a5837478e341ff82d488b2
Trisha Prabhu <trishaprabhu@Trishas-MacBook-Pro.local>
2020-11-04 14:46:08 -0600

    Commit!

--- graph_code.R
+++ graph_code.R
@@ -3,6 +3,27 @@
 library(dplyr)
 library(sentimentr)
 
+# Load the lubridate library; use as.Date to create a new column with the
+# created_at values in MDY format.
+
+library(lubridate)
+trumptweets$newdates <- (as.Date(mdy_hms(trumptweets$created_at)))
+
+newtrumptib <- trumptweets %>%
+  mutate(element_id = 1:2927) %>%
+  group_by(newdates) %>%
+  select(text, newdates, element_id) %>%
+  head(., 500)
+
+trump_ss <- sentiment(get_sentences(trumptweets$text[1:500])) 
+
+senttib <- trump_ss %>%
+  group_by(element_id) %>%
+  summarize(sentimentmeans = mean(sentiment, na.rm = TRUE),
+            .groups = "drop")
+
+graphtib <- inner_join(newtrumptib, senttib, by = "element_id")
+
 # Read in the approval_polllist dataset.
 
 approval_polllist <- read_csv("finalprojectmilestone3-4/approval_polllist.csv")
@@ -15,28 +36,7 @@ enddates <- unique(approval_polllist$enddate)
 #Create an empty column to deposit approval ratings in.
 
 trumptweets$approvalpolls <- c()
-
-# Load the lubridate library; use as.Date to create a new column with the
-# created_at values in MDY format.
-
-library(lubridate)
-trumptweets$newdates <- (as.Date(mdy_hms(trumptweets$created_at)))
-
-newtrumptib <- trumptweets %>%
-  mutate(element_id = 1:2927) %>%
-  group_by(newdates) %>%
-  select(text, newdates, element_id) %>%
-  head(., 100)
-
-trump_ss <- sentiment(get_sentences(trumptweets$text[1:100])) 
-
-senttib <- trump_ss %>%
-  group_by(element_id) %>%
-  summarize(sentimentmeans = mean(sentiment, na.rm = TRUE),
-            .groups = "drop")
-
-graphtib <- inner_join(newtrumptib, senttib, by = "element_id")
-
+  
 #For loop is below:
 
 n <- length(enddates)
